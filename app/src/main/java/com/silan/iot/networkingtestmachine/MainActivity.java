ed1a55bccb026841282abe396965d6eebd364bf8
Li Zheng <flyskywhy@gmail.com>
2015-11-04 16:27:50 +0800

    Add /sdcard/iot/Sync/ to config.xml

--- app/src/main/java/com/silan/iot/networkingtestmachine/MainActivity.java
+++ app/src/main/java/com/silan/iot/networkingtestmachine/MainActivity.java
@@ -15,6 +15,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.unix4j.Unix4j;
+import org.unix4j.unix.grep.GrepOption;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -124,10 +127,29 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
             }
         }
 
-        file = new File("/sdcard/iot/sync-config/config.xml");
+        final String config_xml = "/sdcard/iot/sync-config/config.xml";
+        file = new File(config_xml);
         if (!file.exists()) {
             ShellInterface.runCommand("mkdir -p /sdcard/iot/sync-config/");
             ShellInterface.runCommand(syncthingPath + " -generate=/sdcard/iot/sync-config/");
+        }
+
+        String device_id = Unix4j.fromFile(config_xml)
+                .grep("^        <device id=")
+                .sed("s/^.*id=\"//")
+                .sed("s/\">.*//")
+                .toStringResult();
+
+        String device_id_short = Unix4j.fromString(device_id)
+                .sed("s/-.*//")
+                .toStringResult();
+
+        if ("0" != Unix4j.fromFile(config_xml).grep(GrepOption.count, "\"\\/data\\/Sync\"").toStringResult()) {
+            Unix4j.fromFile(config_xml)
+                    .sed("s/id=\"default\" path=\"\\/data\\/Sync\"/id=\"" + device_id_short + "\" path=\"\\/sdcard\\/iot\\/Sync\"/")
+                    .sed("s/urAccepted>0/urAccepted>-1/")
+                    .toFile(config_xml + ".tmp");
+            ShellInterface.runCommand("mv " + config_xml + ".tmp " + config_xml);
         }
     }
 
