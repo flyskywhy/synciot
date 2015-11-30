cc6743957710231d6944923015b59552fa89289f
Li Zheng <flyskywhy@gmail.com>
2015-11-30 16:48:50 +0800

    server: Add `-version`

--- Go/server/src/synciot/synciot.go
+++ Go/server/src/synciot/synciot.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	Version = "unknown-dev"
+)
+
+// Command line and environment options
+var (
+	showVersion bool
+)
+
+func init() {
+	Version = "0.1"
+}
+
 func main() {
+	flag.BoolVar(&showVersion, "version", false, "Show version")
+	flag.Parse()
+
+	args := os.Args
+	fmt.Println(args)
+
+	if showVersion {
+		fmt.Println(Version)
+		return
+	}
 }
