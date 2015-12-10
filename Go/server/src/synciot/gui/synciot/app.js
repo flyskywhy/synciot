af35cb50f275affbeb30a4cff715b199a426a08c
Li Zheng <flyskywhy@gmail.com>
2015-12-10 13:54:15 +0800

    server: Enhance html with javascript and css

---
+++ Go/server/src/synciot/gui/synciot/app.js
@@ -0,0 +1,8 @@
+// Copyright (C) 2015 Synciot
+
+
+/*jslint browser: true, continue: true, plusplus: true */
+/*global $: false, angular: false, console: false, validLangs: false */
+
+var synciot = angular.module('synciot', [
+]);
